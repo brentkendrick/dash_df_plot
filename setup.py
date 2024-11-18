8e76fd0829e8f07e9621236dc361075e22be35c3
“brentkendrick” <brentkend@gmail.com>
2024-11-17 20:02:04 -0700

    v0.0.19 toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dash_df_plot",
-    version="0.0.18",
+    version="0.0.19",
     packages=find_packages(),
     package_data={
         "dash_df_plot": ["pyproject.toml"],  # Include pyproject.toml in the package
