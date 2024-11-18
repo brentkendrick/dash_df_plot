fe1965e9c03992062d6d3246408c352f1c300a80
“brentkendrick” <brentkend@gmail.com>
2024-11-18 12:04:24 -0700

    v0.0.46 rm flask server

--- dash_df_plot/app.py
+++ dash_df_plot/app.py
@@ -5,7 +5,9 @@ import pandas as pd
 import plotly.express as px
 
 from dash import Dash, dcc, html
-from flask import Flask
+from .config.settings import ASSETS_PATH
+
+# from flask import Flask
 from .utils import __version__
 from .ids import ids
 
@@ -51,18 +53,19 @@ def create_app(dash_url):
     """Using Factory Pattern to instantiate app.  Since we want relative imports to work
     a run.py in the project root is implemented.
     """
-    server = Flask(__name__)
+    # server = Flask(__name__)
 
     app = Dash(
-        server=server,  # type: ignore
+        # server=server,  # type: ignore
         url_base_pathname=dash_url,
+        assets_folder=ASSETS_PATH,
         external_stylesheets=[
             dbc.themes.SPACELAB,
             "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css",
         ],
     )
 
-    session_id = str(uuid.uuid4())
+    # session_id = str(uuid.uuid4())
     # reset_df_stores(session_id)
 
     app.layout = html.Div(
--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from dash_df_plot.app import create_app
 
 app = create_app("/fptrace/")  # Create the Dash app
 
-server = app.server  # Expose the Flask server
+# server = app.server  # Expose the Flask server
 
 if __name__ == "__main__":
     app.run(debug=True)
