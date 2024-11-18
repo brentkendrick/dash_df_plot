1ac144119258d1c38007456e49181850c7db7447
“brentkendrick” <brentkend@gmail.com>
2024-11-17 20:58:18 -0700

    v0.0.31

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dash_df_plot",
-    version="0.0.30",
+    version="0.0.31",
     packages=find_packages(),
     include_package_data=True,  # Include package data from MANIFEST.in
     package_data={
