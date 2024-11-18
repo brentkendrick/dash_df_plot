aa0904219c51e0129e3a78f8e7dc2de24929128b
“brentkendrick” <brentkend@gmail.com>
2024-11-17 21:07:18 -0700

    v0.0.33

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="dash_df_plot",
-    version="0.0.32",
+    version="0.0.33",
     packages=find_packages(),
     include_package_data=True,  # Include package data from MANIFEST.in
-    package_data={
-        "dash_df_plot": ["pyproject.toml", "csv/*"],  # Specify files to include
-    },
+    data_files=[("", ["pyproject.toml"])],  # Include at the package root
 )
